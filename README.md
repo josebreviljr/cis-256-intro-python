602bcd92790101edf36fb8499d6b11bfcd641317
josebreviljr <128244090+josebreviljr@users.noreply.github.com>
2025-08-27 22:03:11 -0500

    Initial commit - CIS 256 Python course materials

---
+++ README.md
@@ -0,0 +1,6 @@
+# CIS 256: Introduction to Python Programming 
+
+This is the repository for all code that I create for the class CIS 256 with the University of Mississippi. CIS 256 is called CSCI for engineering majors, and as a non-engineering major, my course is titled CIS 256.
+
+
+
---
+++ labs/lab0/helloWorld.py
@@ -0,0 +1,14 @@
+"""
+    Name: Jose F. Brevil, Jr.
+    Student ID: 011064137
+    Submission Date: 08.27.25
+    Program: Lab 0
+    Sources Consulted: Runestone Academy - How to Think Like a Computer Scientist: Interactive Edition, CIS 256 Week 1 Slides
+
+    By submitting this work, I attest that it is my original work and that I did not violate
+    the University of Mississippi academic policies set forth in the M book or any
+    policies of the course.
+"""
+print("Hello World!")
+
+
