356db793f319ff8b8cecce48a6991d05eaa2333f
josebreviljr <128244090+josebreviljr@users.noreply.github.com>
2025-12-05 13:38:27 -0600

    Finished prog assignment 4.

--- assignments/assignment4/Jose_Brevil_Program4.py
+++ assignments/assignment4/Jose_Brevil_Program4.py
@@ -13,10 +13,31 @@ import math
 
 def main():
     print("Welcome to the Bone Stress Calculator.")
+    filename = input("Please enter file name: ")
+    bones = []
+    
+    with open(filename, "r") as file: # read the bones.dat file
+        for line in file: 
+            part = line.split()
 
+            bone_type = part[0]
+            name = part[1]
+            length = float(part[2])
+            outer_radius = float(part[3])
+            inner_radius = float(part[4])
+            moment = float(part[5])
 
+            bone = Bone(name, length, outer_radius, inner_radius, moment, bone_type)
+            bones.append(bone)
+            
+    for bone in bones:
+        print("Bone Information: ")
+        print("-" * 20)
+        bone.display_info()
+        print("_" * 20)
+    
 
-class Bone():
+class Bone(): # class Bone
 
     def __init__(self, name, length, outer_radius, inner_radius, moment, bone_type): 
        self.__name = name
@@ -32,13 +53,29 @@ class Bone():
         inertia = (math.pi / 4) * (ro ** 4 - ri ** 4)
         return inertia
 
-
-
     def calculate_bending_stress(self):
-        print("yes")
+        I = self.calculate_moment_of_inertia()
+        c = self.__outer_radius
+        M = self.__moment
+        step1 = (M * c) / I
+        step2 = step1 / 1000000
+        return step2
 
-    def display_info(self):
-        print("Yes")
+    def display_info(self): # display, calculate status
+
+        if self.calculate_bending_stress() >= 170:
+            status = "SAFE"
+        else:
+            status = "WARNING – RISK OF FRACTURE"
+
+        print(f"Bone: {self.__bone_type} {self.__name}")
+        print(f"Length: {self.__length:.3f} m")
+        print(f"Outer Radius: {self.__outer_radius:.4f} m")
+        print(f"Inner Radius: {self.__inner_radius:.4f} m")
+        print(f"Moment: {self.__moment:.2f} N * m")
+        print(f"Moment of Inertia: {self.calculate_moment_of_inertia():.2f} m^4")
+        print(f"Bending Stress: {self.calculate_bending_stress()} MPa")
+        print(f"Status: {status}")
 
 
 if __name__ == "__main__":
